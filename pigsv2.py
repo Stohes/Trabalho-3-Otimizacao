cc9169dec3f7a4a40250f0a52ee1598c2c2c5cf4
Thiago Claas <t.chies@edu.pucrs.br>
2022-10-18 19:29:01 -0300

    Acho que pega todas as possibilidades

--- pigsv2.py
+++ pigsv2.py
@@ -6,36 +6,41 @@ tabuleiro = [["x" for _ in range(tamanho)] for _ in range(tamanho)]
 
 tabuleirosDasSolucoesDiferentes = []
 
-maximoDePorcos = 4
+porcosDesejados = 2
 
+linhas = set()
 colunas = set()
 diagonaisPositivas = set()
 diagonaisNegativas = set()
 
 
-def rainhasDiferenciado(linha, maximoDePorcos, porcosNoTabuleiro=0):
-    if maximoDePorcos == porcosNoTabuleiro:
-        tabuleirosDasSolucoesDiferentes.append(copy.deepcopy(tabuleiro))
-        return
+def rainhasDiferenciado(porcosDesejados, porcosNoTabuleiro=0):
+    if porcosDesejados == porcosNoTabuleiro:
+        if tabuleiro not in tabuleirosDasSolucoesDiferentes:
+            tabuleirosDasSolucoesDiferentes.append(copy.deepcopy(tabuleiro))
+            return
+    
+    for linha in range(len(tabuleiro)):
+        for coluna in range(len(tabuleiro)):
+            if linha in linhas or coluna in colunas or (linha + coluna) in diagonaisPositivas or (linha - coluna) in diagonaisNegativas:
+                continue
+            
+            linhas.add(linha)
+            colunas.add(coluna)
+            diagonaisPositivas.add(linha + coluna)
+            diagonaisNegativas.add(linha - coluna)
+            tabuleiro[linha][coluna] = "Q"
 
-    for coluna in range(len(tabuleiro)):
-        if coluna in colunas or (linha + coluna) in diagonaisPositivas or (linha - coluna) in diagonaisNegativas:
-            continue
-        
-        colunas.add(coluna)
-        diagonaisPositivas.add(linha + coluna)
-        diagonaisNegativas.add(linha - coluna)
-        tabuleiro[linha][coluna] = "Q"
-
-        rainhasDiferenciado(linha + 1, maximoDePorcos, porcosNoTabuleiro + 1)
-        
-        colunas.remove(coluna)
-        diagonaisPositivas.remove(linha + coluna)
-        diagonaisNegativas.remove(linha - coluna)
-        tabuleiro[linha][coluna] = "x"
+            rainhasDiferenciado(porcosDesejados, porcosNoTabuleiro + 1)
+            
+            linhas.remove(linha)
+            colunas.remove(coluna)
+            diagonaisPositivas.remove(linha + coluna)
+            diagonaisNegativas.remove(linha - coluna)
+            tabuleiro[linha][coluna] = "x"
         
 
-rainhasDiferenciado(0, 4)
+rainhasDiferenciado(porcosDesejados)
 
 for tabuleiro in tabuleirosDasSolucoesDiferentes:
     print(tabulate(tabuleiro, tablefmt='fancy_grid'))
